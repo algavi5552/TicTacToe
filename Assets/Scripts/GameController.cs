2d7fb53e708864c82dd0ea2ab80eebac4adf9e9f
algavi5552 <56834981+algavi5552@users.noreply.github.com>
2020-03-11 16:26:23 +0300

    Add files via upload
    для 2х игроков уже работает, еще нет игры с компом и меню\рестарта игры

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text[] buttonList;
+    private string playerSide;
+
+    private void Awake()//загрузка экземпляра сценария
+    {
+        SetGameControllerReferenceOnButtons();
+        playerSide = "X"; //первым ходит Х
+    }
+
+    void SetGameControllerReferenceOnButtons()
+    {
+        for(int i = 0; i < buttonList.Length; i++)//перебираем все кнопки
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);//читаем их значение
+        }
+    }
+    public string GetPlayerSide()
+    {
+        return playerSide;
+    }
+
+    public void EndTurn()//конец хода, проверка на победу
+        //простите за жуткий код, у автора видеоуроков он еще хуже, а времени осталось 1 сутки до сдачи=)
+    {
+        if (
+            (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+         || (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+         || (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+         || (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+         || (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+         || (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+         || (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+         || (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+           )
+        {
+            GameOver();
+        }
+         ChangeSides();
+
+    }
+
+    void GameOver()
+    {
+        for(int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;//отключаем возможность юзать все кнопки
+        }
+        
+    }
+
+    void ChangeSides()
+    {
+        playerSide = (playerSide == "X") ? "O" : "X";
+    }
+}
--- Assets/Scripts/GridSpace.cs
+++ Assets/Scripts/GridSpace.cs
@@ -7,11 +7,19 @@ public class GridSpace : MonoBehaviour
 {
     public Button button;
     public Text buttonText;
-    public string PlayerSide;
+
+    private GameController gameController;
 
     public void SetSpace()
     {
-        buttonText.text = PlayerSide;
-        button.interactable = false;
+        buttonText.text = gameController.GetPlayerSide();
+        //ставим в клетку х или 0, в зависимости от СторонаИгрока
+        button.interactable = false;//отключаем возможность юзать эту кнопку
+        gameController.EndTurn();//завершаем ход
+    }
+
+    public void SetGameControllerReference( GameController controller)
+    {
+        gameController = controller;
     }
 }
