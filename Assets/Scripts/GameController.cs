11e1c1ec1f9bd94152aa947729f7225d6ab62441
algavi5552 <56834981+algavi5552@users.noreply.github.com>
2020-03-11 17:24:45 +0300

    Add files via upload
    реализовали ничью

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,11 +11,13 @@ public class GameController : MonoBehaviour
     public GameObject gameOverPanel;
     public Text gameOverText;
 
+    private int moveCount;//число ходов общее
     private void Awake()//загрузка экземпляра сценария
     {
         gameOverPanel.SetActive(false);//отключим меню в начале игры
         SetGameControllerReferenceOnButtons();
         playerSide = "X"; //первым ходит Х
+        moveCount = 0;
     }
 
     void SetGameControllerReferenceOnButtons()
@@ -33,6 +35,8 @@ public class GameController : MonoBehaviour
     public void EndTurn()//конец хода, проверка на победу
         //простите за жуткий код, у автора видеоуроков он еще хуже, а времени осталось 1 сутки до сдачи=)
     {
+        moveCount++;
+
         if (
             (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
          || (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
@@ -46,8 +50,14 @@ public class GameController : MonoBehaviour
         {
             GameOver();
         }
-         ChangeSides();
 
+        if(moveCount >= 9)
+        {
+            SetGameOverText("no one wins");
+        }
+
+        ChangeSides();
+      
     }
 
     void GameOver()
@@ -55,8 +65,7 @@ public class GameController : MonoBehaviour
         for(int i = 0; i < buttonList.Length; i++)
         {
             buttonList[i].GetComponentInParent<Button>().interactable = false;//отключаем возможность юзать все кнопки
-            gameOverPanel.SetActive(true);//вызываем меню игры
-            gameOverText.text = playerSide + " wins";
+            SetGameOverText(playerSide + " wins");
         }
         
     }
@@ -64,5 +73,11 @@ public class GameController : MonoBehaviour
     void ChangeSides()
     {
         playerSide = (playerSide == "X") ? "O" : "X";
+    }
+
+    void SetGameOverText(string value)//какой текст передадим, тот и напишет в меню
+    {
+        gameOverPanel.SetActive(true);//вызываем меню игры
+        gameOverText.text = value;
     }
 }
